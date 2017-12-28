447523d67e4fbe80eb2a4cd71565210ce4d135bb
Sabrina Izcovich <sizcovich@dc.uba.ar>
2017-12-28 11:53:43 -0300

    Design

--- newTab.js
+++ newTab.js
@@ -6,7 +6,10 @@ window.onload = function() {
     // element name
     var name = document.getElementById('name');
     //element example
-    var exa = document.getElementById('example');
+    var exa = document.getElementById('example-content');
+    // var resultViewer = document.getElementById('example-viewer');
+    var moreInfo = document.getElementById('link');
+    var code = document.getElementById('code-container');
 
     name.innerHTML = db[rand].name;
     if(db[rand]['description'] != ''){
@@ -14,19 +17,19 @@ window.onload = function() {
     }
     if(db[rand]['example'].code != null){
         if(db[rand].example.title !== ""){
-            exa.innerHTML+= "<h4 id=example-title>" + db[rand].example.title + "</h4>"
+            exa.innerHTML+= "<h2 id=example-title>" + db[rand].example.title + "</h2>"
         }
         else{
-            exa.innerHTML += "<p>" + db[rand].example.code.replace(/</g, "&lt;") + "</p>"
+            code.innerHTML += "<p id=code-example>" + db[rand].example.code.replace(/</g, "&lt;") + "</p>"
         }
         if(db[rand].example.description !== ""){
             exa.innerHTML+= "<p id=example-description>" + db[rand].example.description + "</p>"
         }
         
         if (db[rand].example.show === "yes"){
-            exa.innerHTML+="<pre id=code>"+ db[rand].example.code + "</pre>";
+            exa.innerHTML+="<div class=code>"+ db[rand].example.code + "</div>";
         }
-        exa.innerHTML+="<a id=reference href="+ db[rand].reference +">Más info</a>";
+        moreInfo.href+= db[rand].reference;
     }
 
 	//first time
